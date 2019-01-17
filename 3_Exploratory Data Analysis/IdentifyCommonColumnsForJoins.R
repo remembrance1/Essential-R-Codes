239b74b1419e16160a689c4557975b1e449703ad
remembrance1 <javierngkh@gmail.com>
2019-01-17 18:33:58 +0800

    updated identification of common columns for joins

--- 3_Exploratory Data Analysis/IdentifyCommonColumnsForJoins.R
+++ 3_Exploratory Data Analysis/IdentifyCommonColumnsForJoins.R
@@ -4,7 +4,8 @@
 #    Date: 15/1/2019                                                                                      #
 #    Libraries Used: -                                                                                    #
 ###########################################################################################################
-
+library(dplyr)
+library(purrr)
 
 df1 <- data.frame(fruit=c("apple", "Orange", "Pear"), location = c("Japan", "China", "Nigeria"), price = c(32,53,12))
 df2 <- data.frame(grocery = c("Durian", "Apple", "Watermelon"), 
@@ -12,12 +13,25 @@ df2 <- data.frame(grocery = c("Durian", "Apple", "Watermelon"),
                   name = c("Mark", "John", "Tammy"), 
                   favourite.food = c("Apple", "ORANGE", "Cakes"), 
                   invoice = c("XD1", "XD2", "XD3"))
-df3 <- data.frame(address=c("address1", "address2", "address3"), location = c("USA", "UK", "China"))
+df3 <- data.frame(address=c("address1", "address2", "address3"), location = c("USA", "UK", "China"), test = c("a", "apple", "orange"))
 df4 <- data.frame(a=c("no", "no"), b=c("yes", "yes"))
 
-#set df1 as table1 and df2 as table 2. Table 2 will appear as the column headers.
+#setting null list
+templist <- list() #create templist
+newlist <- list() #create newlist
 
-foridentification_temp <- function(df1, df2){
+#Get environment objects in decending order
+allobj <- ls()[sapply(ls(), function(i) class(get(i))) == "data.frame"]
+allobj_new <- list()
+for (i in 1:length(allobj)){ #loop to identify obtain vector of dataframes to loop through
+  if (i <= length(allobj) - 1){
+    allobj_new[[i]] <- allobj[(i):(as.numeric(length(allobj)))]
+  }
+}
+
+####################################FUNCTIONS###################################################
+#set df1 as table1 and df2 as table 2. Table 2 will appear as the column headers.
+foridentification <- function(df1, df2){
   vec <- sapply(names(df1), function(x) {
     temp <- sapply(names(df2), function(y) 
       if(any(match(tolower(df1[[x]]), tolower(df2[[y]]), nomatch = FALSE))) y else NA) #works for lower/upper case strings, as long as there is a match in char
@@ -28,24 +42,40 @@ foridentification_temp <- function(df1, df2){
   return(output)
 }
 
-# foridentification <- function(df1, df2){ #function to add respective column names #redundant
-#   temp <- foridentification_temp(df1,df2)
-#   names(temp)[1] <- paste("From", deparse(substitute(df1)))
-#   names(temp)[2] <- paste("From", deparse(substitute(df2)))
-#   return(temp)
-# }
-
-#loop to get table1 matched against tabl2, tabl3..
-allobj <- ls()[sapply(ls(),function(t) is.data.frame(get(t)))]
-templist <- list() #create templist
-
-for (i in 1:length(allobj)){
-        if (i < length(allobj))
-          templist[[i]] <- foridentification_temp(get(allobj[1]), get(allobj[i+1])) 
+#rename columns in list
+renamecol_combine <- function(list_here, refvec){
+  output <- lapply(seq_along(list_here), function(x) {
+    names(list_here[[x]])[1] <- paste0("From ",refvec[1]) #rename 1st column
+    names(list_here[[x]])[2] <- paste0("Against ",refvec[x+1]) #rename 2nd column
+    list_here[[x]]
+  })
+  return(output %>% reduce(left_join, by = paste0("From ",refvec[1])))
 }
 
-#####################TRYING TO RENAME COLUMN IN A LIST#################################
-#rename columns in list..
-lapply(templist, function(x) {names(x)[2] <- paste("From", allobj[seq(1,2)]); x})
+#loop to get table1 matched against tabl2, tabl3.. (WILL OUTPUT A LIST IN THE END!)
+insertref_obj <- function(refobj){
+  for (i in 1:length(refobj)){
+    if (i < length(refobj)){
+      templist[[i]] <- foridentification(get(refobj[1]), get(refobj[i+1])) 
+    } 
+  } 
+  return(templist) #returns a list
+}
 
+#renamecol_combine(insertref_obj(allobj[2:4]), allobj[2:4])
+
+################################################################################################
+renamecol_combine(insertref_obj(allobj_new[[2]]), allobj_new[[2]]) 
+
+ultimatefunction <- function(env_obj_list){
+  for (i in 1:length(env_obj_list)){
+    newlist[[i]] <- renamecol_combine(insertref_obj(env_obj_list[[i]]), env_obj_list[[i]])
+  }
+  return(newlist)
+}
+                                                 
+t1 <- ultimatefunction(allobj_new) #need to save each dataframe from the list into a sheet in workbook
+sink("output.txt")
+print(t1)
+sink()
 
