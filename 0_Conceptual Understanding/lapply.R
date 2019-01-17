8490882b84146ecdc96c7eab99e4a99f86efdc78
remembrance1 <javierngkh@gmail.com>
2019-01-17 18:31:33 +0800

    updated lapply

--- 0_Conceptual Understanding/lapply.R
+++ 0_Conceptual Understanding/lapply.R
@@ -3,17 +3,31 @@ list.df <- list(data.frame(a=(1:5), b=(6:10)), data.frame(a=(1:5), b=(11:15)), d
 #will square all dataframes in list, at column 2, and only return a vector
 lapply(list.df, function(x) x[,2]^2) 
 
-#returns a list instead...
-out <- lapply(list.df, function(x) {
+#returns a list instead...            
+lapply(list.df, function(x) {
   i <- seq_along(list.df)
   x[i, 2] <- x[i, 2] ^ 2
   x
 })
 
-#change column names in list
+######FOR UNDERSTANDING########
+The (X) in the function within lapply refers to the index!
 
+########################################################################################################
 allorg <- c("df1", "df2", "df3", "df4")
 
+#lapply to rename columns and iterating through another vector (allobj), to sequentially add column names
+lapply(seq_along(templist), function(x) {
+  names(templist[[x]])[2] <- allorg[x]
+  templist[[x]]
+})
+
+#mapply can be used instead
+mapply(function(x, y) {names(x)[2] <- y; x}, templist, allobj) #templist is assigned to x and allobj is assigned to y
+#mapply it takes 1st list from templist and 1st value from allobj and assign the names. 
+#In the next iteration it takes second list from templistand 2nd value from allobj and so on. -
+
+#change column names in list
 for (i in seq_along(list.df)){
   colnames(list.df[[i]]) <- c("df", paste0("df",i+1)) #will change the column name of each dataframe in a list in seq
 }
