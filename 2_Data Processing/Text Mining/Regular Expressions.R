4427b28162ea5a977cf53858cf69c15126c38075
remembrance1 <javierngkh@gmail.com>
2019-01-28 17:51:11 +0800

    Updated regex tutorial

--- 2_Data Processing/Text Mining/Regular Expressions.R
+++ 2_Data Processing/Text Mining/Regular Expressions.R
@@ -77,10 +77,75 @@ grep(pattern = "[0-9]", numerics, value = TRUE)
 grep(pattern = "[^0-9]", numerics, value = TRUE)
 ## [1] "17-April" "I-II-III" "R 3.0.1"
 
+#########################################################################
 x <- "ABC|DEF|GHI"
 gsub("\\|.*", "", x)
 
-x <- "12/3/2019"
-pattern <- regex("\\d{1,2}\\/\\d{1,2}\\/\\d{4}", comments = T)
+#extracts out a fixed pattern even in the midst of a complex sentence
+x <- "hello the date *$(@) is this 12/3/2019 this is a series of test"
+pattern <- regex("\\d{1,2}\\/\\d{1,2}\\/\\d{2,4}", comments = T)
 str_extract(x, pattern)
 
+#testing the above via a dataframe
+test_vec <- data.frame(test = c("hi11/4/2018", "mucks3/09/2014"))
+str_extract(test_vec$test, pattern)
+
+#extracting letters before a fullstop sign (method 1)
+x <- "hello.csv"
+pattern <- regex("[^.]*") #[^] refers to anything but.
+str_extract(x, pattern)
+
+#subsetting all the letters before a sign (Method 2)
+x <- "hello_name_hi"
+pattern <- regex("^(\\S+?)\\_")
+str_extract(x, pattern)
+
+#subsetting all the letters before a sign (Method 3)
+str_extract(x, "[a-z]*")
+
+#use this to split up the vector
+strsplit(x, "_")
+
+#extracting 1st vector before comma
+x <- data.frame(cities = c("Singapore, Aussie, NZ"))
+gsub(",.*$", "\\1", x$cities) #? refers to 0 or 1 match, * = 0 or more matches, + = one or more match
+
+########################################################################
+#subsetting rows that matches a portion of a string
+x <- c("hello.csv", "hey.xlsx")
+str_subset(x, "\\.csv")
+#[1] "hello.csv"
+
+#what if I want to subset out rows that START with a certain string? 
+str_subset(x, "^h") #we use the ^ symbol
+
+#what if I want to subset out rows that END with a certain string? 
+str_subset(x, ".csv$") #we use the $ symbol
+
+#what if I want to subset out rows based on certain conditions?
+x <- c("hello.csv", "hey.xlsx", "useless.jpg")
+str_subset(x, "\\.(csv|xlsx)$")
+
+#what if i want to subset specific rows only with multiple conditions? 
+files = c(
+  "tmp-project.csv", "project.csv", 
+  "project2-csv-specs.csv", "project2.csv2.specs.xlsx", 
+  "project_cars.ods", "project-houses.csv", 
+  "Project_Trees.csv","project-cars.R",
+  "project-houses.r", "project-final.xls", 
+  "Project-final2.xlsx"
+)
+
+#condition 1: extract out all those .csv that starts with p only and one _
+str_subset(files, "(P|p)roject*\\.csv$") #<------ This does not work as it extracts only those with project.csv, and skips special symbols
+str_subset(files, "^(P|p)roject(\\_|\\-)[a-zA-Z]*\\.(csv)$") #<---- This works
+
+######### VERY USEFUL REGULAR EXPRESSION INFORMATION ###################################
+#extract out happy to learn.com but then we dont have to code out everything... 
+x <- c("happy_to-learn.com", "His_is-omitted.net")
+str_subset(x, "^[a-zA-Z]+(\\_|\\-).*\\.com$")
+
+# We need to specify one or more with + as the _ or - are not just after the first letter.
+# str_subset(x, "^[a-zA-Z]+(\\_|\\-).*\\.com$")
+# #[1] "happy_to-learn.com"
+# Also, the .* refers to zero or more characters as . can be any character until the . and 'com' at the end ($) of the string
