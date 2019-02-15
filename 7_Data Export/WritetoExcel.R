ba55b905acb8b3ceade732869badb4cd2b2b77b0
remembrance1 <javierngkh@gmail.com>
2019-02-15 13:06:33 +0800

    updated excel export

--- 7_Data Export/WritetoExcel.R
+++ 7_Data Export/WritetoExcel.R
@@ -1,14 +1,19 @@
+## To export our a dataframe into excel sheet
+## Use Openxlsx
+
 #################################################
-#FREESTYLE
 library(openxlsx)
 options("openxlsx.minWidth" = 3)
 options("openxlsx.maxWidth" = 300) ## 
 ## create a workbook and add a worksheet
 wb <- createWorkbook()
-addWorksheet(wb, "Receipt")
+
+##This will only create one excel sheet, if want more, need to duplicate the lines
+addWorksheet(wb, "Sheet1")
 hs1 <- createStyle(fgFill = "#4F81BD", halign = "CENTER", textDecoration = "Bold", fontColour = "white", border = "Bottom")
 writeData(wb, 1, all.files, startRow = 1, startCol = 1, headerStyle = hs1,
           borders = "surrounding", borderStyle = "medium")
-setColWidths(wb, "Receipt", cols = 1:ncol(all.files), widths = "auto")
+setColWidths(wb, "Sheet1", cols = 1:ncol(all.files), widths = "auto")
+##
 
-saveWorkbook(wb, "Excel_List.xlsx", TRUE)
+saveWorkbook(wb, "nameofexcelworkbook.xlsx", TRUE)
